1b6c27d59a8515f5c7119f71819e3994648bd32d
xiepeng <xiepeng@ibobei.cn>
2019-10-11 10:30:51 +0800

    初始化

---
+++ gateway/main/java/com/x/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.x.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xiepeng
+ * @version 1.0
+ * @date 2019年10月11日09:48:11
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
